7e7c27cf9faa9b0262a75043c480c84a9bb7b8c1
DiNg <dinhquangnguyen1997@gmail.com>
2026-09-15 10:22:42 +0700

    feat: integrate Firebase and add Firebase Analytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -58,4 +59,6 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(kotlin("test"))
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 }
